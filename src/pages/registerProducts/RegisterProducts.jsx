39eb37ce6dcce39f10187b827f993ee8366a52b2
Ivan Reis <ivanreisdev@gmail.com>
2023-12-16 00:25:02 -0300

    Atualização

---
+++ src/pages/registerProducts/RegisterProducts.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+const RegisterProducts = () => {
+  return (
+    <div className='register'>
+
+    <form>
+        <label htmlFor="name">Produto *</label>
+        <input type="text" name="name" id="name" placeholder='Digite o nome do pruduto' required/>
+
+        
+
+    </form>
+    </div>
+  )
+}
+
+export default RegisterProducts
